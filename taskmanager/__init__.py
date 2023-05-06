4597307c542cac28bd9d1d28220b0740312d5e6d
sven-cork <linslus81@gmail.com>
2023-05-06 13:13:09 +0000

    Create database tables

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,13 +1,15 @@
+
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLAlCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import routes
+from taskmanager import routes  # noqa
